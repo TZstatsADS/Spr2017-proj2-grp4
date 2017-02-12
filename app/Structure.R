dc354595ea17afce3bc23dc68075c1dd9b17c4c0
bz2290 <bz2290@columbia.edu>
2017-02-11 19:32:35 -0500

    structure update

--- app/Structure.R
+++ app/Structure.R
@@ -2,7 +2,7 @@ library(shiny)
 
 shinyApp(
 ui = fluidPage(
-  navbarPage("Our App Names",
+  navbarPage("Our App's Name",
               tabPanel("First tab name",
                        absolutePanel(top = 20,
                                      left = 20,
@@ -12,13 +12,14 @@ ui = fluidPage(
                                      cursor = "move",
                                      wellPanel(
                                        sliderInput("n", "", min=3, max=20, value=5)
+                                              )
                                      )
-                       )
                        ),
               tabPanel("Second tab name"),
               tabPanel("Thrid tab name",
                        absolutePanel(top = 20,
-                                     left = 20,
+                                     left = 300,
+                                     height = 600,
                                      draggable = TRUE,
                                      wellPanel(
                                        sliderInput("n","",min = 5, max = 20,value=5)
