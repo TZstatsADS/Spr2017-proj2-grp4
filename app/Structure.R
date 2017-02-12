440eb713821db6d12fd313990e6e036a009812f4
bz2290 <bz2290@columbia.edu>
2017-02-12 17:38:30 -0500

    structure update

--- app/Structure.R
+++ app/Structure.R
@@ -5,22 +5,43 @@ shinyApp(
 ui = fluidPage(
   navbarPage("Our App's Name",
               tabPanel("First tab name",
-                       absolutePanel(top = 20,
+                       absolutePanel(top = 50,
                                      left = 20,
                                      width = 300,
                                      height = 600,
                                      draggable = TRUE,
                                      cursor = "move",
                                      wellPanel(
-                                       sliderInput("n", "", min=3, max=20, value=5)
+                                       #sliderInput("stat","start Comparison",min=1,max=20,step=1,value =1)
+                                       selectInput("major","Your Major",choices = c("NULL","Major1","Major2"),selected = "NULL"),
+                                       numericInput("score.sat","Your SAT Scores",value=0,min=20,max=100),
+                                       numericInput("score.act","Your ACT Scores",value=0,min=0,max=36),
+                                       radioButtons("cost","Preferred Cost of Attendence",choices=c("NULL","$2000-$2999","$3000-$3999"),selected = "NULL"),
+                                       checkboxGroupInput("stat","Start Comparison!",choices="Show stats!",selected = NULL)
                                               ),
-                                     style = "opacity: 0.5"
-                                     )
+                                     style = "opacity: 0.9"
+                                     ),
+                       conditionalPanel("input.stat =='Show stats!'",
+                         absolutePanel(top = 50,
+                                       right = 20,
+                                       width =300,
+                                       height = 10000,
+                                       draggable = TRUE,
+                                       cursor = "move",
+                                       wellPanel(
+                                        sliderInput("input","BLBLABLA",min =1 ,max =20 ,step =1, value =1) 
+                                                ),
+                                       style = "opacity: 0.9"
+                                       )
+                                        )
+                                  
+                       
+                                     
                        ),
               tabPanel("Second tab name"),
               tabPanel("Data Reference",
                        absolutePanel(top = 20,
-                                     left = 300,
+                                     left = 20,
                                      height = 600,
                                      draggable = TRUE,
                                      wellPanel(
