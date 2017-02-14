5b92a7979ed8f6d31dcc5bf4a16164c5100dd847
bz2290 <bz2290@columbia.edu>
2017-02-13 21:39:50 -0500

    update

--- app/Structure.R
+++ app/Structure.R
@@ -1,26 +1,34 @@
 #Structure of our APP:
+#Have not been able to dynamically change the opacity of the panel. 
 library(shiny)
+library(ggmap)
 
 shinyApp(
 ui = fluidPage(
   navbarPage("Our App's Name",
               tabPanel("Locate Your School!",
-                       absolutePanel(top = 50,
-                                     left = 20,
-                                     width = 300,
-                                     height = 600,
-                                     draggable = TRUE,
-                                     cursor = "move",
+                       sidebarLayout(
+                         sidebarPanel(  
+                       #absolutePanel(top = 50,
+                        #             right = 20,
+                         #            width = 300,       Remove these comments to initate moveable panel
+                          #           height = 600,
+                           #          draggable = TRUE,
+                                     #cursor = "move",
                                      wellPanel(
                                        #sliderInput("stat","start Comparison",min=1,max=20,step=1,value =1)
                                        selectInput("major","Your Major",choices = c("NULL","Major1","Major2"),selected = "NULL"),
                                        numericInput("score.sat","Your SAT Scores",value=0,min=20,max=100),
                                        numericInput("score.act","Your ACT Scores",value=0,min=0,max=36),
                                        radioButtons("cost","Preferred Cost of Attendence",choices=c("NULL","$2000-$2999","$3000-$3999"),selected = "NULL"),
-                                       checkboxGroupInput("stat","Start Comparison!",choices="Show stats!",selected = NULL)
-                                              ),
-                                     style = "opacity: 0.9"
-                                     ),
+                                       checkboxGroupInput("stat","Start Comparison!",choices="Show stats!",selected = NULL),
+                                       sliderInput("Alt","Altitude",min=40.5,max=45.04,step = 0.01,value = 41),
+                                       sliderInput("Long","Longitude",min=-80.52,max=-71.95,step = 0.01,value=-73)
+                                               )#Do not forget to add comma, if you want to initate moveable panel.
+                            #         style = "opacity: 0.9"
+                             #        )
+                                      ),
+                       mainPanel(
                        conditionalPanel("input.stat =='Show stats!'",
                          absolutePanel(top = 50,
                                        right = 20,
@@ -33,10 +41,12 @@ ui = fluidPage(
                                                 ),
                                        style = "opacity: 0.9"
                                        )
-                                        )
+                                        ),
                                   
                        
-                                     
+                         leafletOutput("map")
+                               )
+                       )
                        ),
               tabPanel("Comparision!"),
               tabPanel("Historical Data Overview"),
@@ -55,9 +65,26 @@ ui = fluidPage(
             )
   
                 ),
-server = function(input, output){}
+server = function(input, output){
+  map.plot.date = reactive({
+    
+    data.frame(alt = input$Alt,
+               long = input$Long
+              )
+                          })
+  
+  output$map = renderLeaflet({
+    leaflet() %>%
+      setView(lng = -74, lat = 42, zoom = 6) %>%
+      addTiles() %>%
+      addCircleMarkers(lng = map.plot.date()$long, lat = map.plot.date()$alt, popup = c("Testing For Project"))
+                         })
+                                }
 )
 
 
-
-
+#Add Multiple marker on map
+#map.1 = leaflet() %>%
+ # setView(lng = -74, lat = 42, zoom = 6) %>%
+  #addTiles() %>%
+  #addCircleMarkers(lng = c(-74.121212121,-73.12313212312313), lat = c(42.1231212123,41.12345738453), popup = c("HAHA","haskjdhasd"))
